6fb45e9d8aae849f7ccb7a5cb2cc804a2768e087
Christoph Weise-Onnen <cwo@thepeaklab.com>
2018-11-21 17:56:54 +0100

    added components

--- src/TextField/index.js
+++ src/TextField/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
-import {inputProps} from '../Input';
-import {inputLabelProps} from '../InputLabel';
-import {formHelperTextProps} from '../FormHelperTextProps';
-import {formControlProps} from '../FormControl';
+import { inputProps } from "../Input";
+import { inputLabelProps } from "../InputLabel";
+import { formHelperTextProps } from "../FormHelperText";
+import { formControlProps } from "../FormControl";
 
 export default ({ ...rest }) => (
-  <TextField classes={formControlProps} InputProps={inputProps} InputLabelProps={inputLabelProps} FormHelperTextProps={formHelperTextProps} {...rest} />
+  <TextField
+    classes={formControlProps}
+    InputProps={inputProps}
+    InputLabelProps={inputLabelProps}
+    FormHelperTextProps={formHelperTextProps}
+    {...rest}
+  />
 );
--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 export { default as AppBar } from "./AppBar";
+export { default as FormControl } from "./FormControl";
+export { default as FormHelperText } from "./FormHelperText";
+export { default as Input } from "./Input";
+export { default as InputLabel } from "./InputLabel";
 export { default as TextField } from "./TextField";
