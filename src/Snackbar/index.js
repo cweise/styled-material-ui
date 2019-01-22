4a2c4435d65b672fe16a58b45402e4012cb503b5
Dennis Schneider <ds@thepeaklab.com>
2019-01-22 16:09:17 +0100

    small fix

--- src/Snackbar/index.js
+++ src/Snackbar/index.js
@@ -3,12 +3,11 @@ import Snackbar from "@material-ui/core/Snackbar";
 import { styles } from "@material-ui/core/Snackbar/Snackbar";
 
 import { getClasses, getStyles } from "../utils";
-import { menuProps } from "../Menu";
 
 export const snackbarProps = {
   classes: getClasses(getStyles(styles))
 };
 
 export default ({ ...rest }) => (
-  <Snackbar {...snackbarProps} MenuProps={{ ...menuProps }} {...rest} />
+  <Snackbar {...snackbarProps} {...rest} />
 );
--- src/SnackbarContent/index.js
+++ src/SnackbarContent/index.js
@@ -3,12 +3,11 @@ import SnackbarContent from "@material-ui/core/SnackbarContent";
 import { styles } from "@material-ui/core/SnackbarContent/SnackbarContent";
 
 import { getClasses, getStyles } from "../utils";
-import { menuProps } from "../Menu";
 
 export const snackbarContentProps = {
   classes: getClasses(getStyles(styles))
 };
 
 export default ({ ...rest }) => (
-  <SnackbarContent {...snackbarContentProps} MenuProps={{ ...menuProps }} {...rest} />
+  <SnackbarContent {...snackbarContentProps} {...rest} />
 );
